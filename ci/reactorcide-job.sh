fd4f7838f5197c66f66ea20d4ffdc0ed8ea9b5b0
Tod Hansmann <githubpub@todandlorna.com>
2025-01-16 22:52:56 -0700

    feat: switch to pysocha-sites helm chart

---
+++ ci/reactorcide-job.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "I am in CI in the innner root job script"
+
+mkdir -p .kube
+touch .kube/config
+echo "${CI_KUBECONFIG}" > .kube/config
+
+helm repo add catalyst-helm https://raw.githubusercontent.com/catalystcommunity/charts/main
+helm repo update
+
+VERSION="$(cat ${REACTORCIDE_REPOROOT}/content/extra_files/VERSION.txt)"
+
+# Now Helm Chart
+helm upgrade \
+  --install \
+  --create-namespace \
+  --namespace tnl-site \
+  tnl-site \
+  --set image.tag=${VERSION} \
+  -f ${REACTORCIDE_REPOROOT}/values.yaml catalyst-helm/pysocha-site \
+  --version 1.0.1
---
+++ ci/release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+touch runnerenv.sh
+echo "REACTORCIDE_JOB_REPO_URL=${REACTORCIDE_JOB_REPO_URL}" >> runnerenv.sh
+
+touch jobenv.sh
+echo "REACTORCIDE_JOB_ENTRYPOINT=${REACTORCIDE_JOB_ENTRYPOINT}" >> jobenv.sh
+echo "CI_KUBECONFIG=\"${CI_KUBECONFIG}\"" >> jobenv.sh
+
+touch cisshkey
+echo "${CI_SSH_KEY}" >> cisshkey
+chmod 600 cisshkey
+scp -i cisshkey -o "StrictHostKeyChecking=no" -P ${CI_HOST_PORT} runnerenv.sh jobenv.sh ${CI_HOST_USER}@${CI_HOST_ADDRESS}:~/
+# external-root.sh should already exist per reactorcide requirements
+ssh ${CI_HOST_USER}@${CI_HOST_ADDRESS} -i cisshkey -o "StrictHostKeyChecking=no" -p ${CI_HOST_PORT} ./external-root.sh
